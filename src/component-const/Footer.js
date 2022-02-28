02dd69ed786be0a0eedad0bb5876fa306501ad3e
Lucie Cayment <lcayment@ensc.fr>
2022-02-28 16:59:00 +0100

    add logo

--- src/component-const/Footer.js
+++ src/component-const/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Footer.css";
+import logotw from "../img/icon-twitter.svg";
+import logofb from "../img/icon-fb.svg";
+import logolinkedin from "../img/icon-linkedin.svg";
+import logoyt from "../img/icon-yt.svg";
 
 function Header() {
   return (
@@ -7,7 +11,16 @@ function Header() {
       <div className="Footer-Links">Admin</div>
       <div className="Footer-Links">Crédits</div>
       <div className="Footer-Links">Contact</div>
-      <div className="Links">TW, YT, FB, LK</div>
+      <div className="Links">
+        <img src={logotw} className="Footer-logo" alt="logo-tw"></img>
+        <img src={logofb} className="Footer-logo" alt="logo-fb"></img>
+        <img
+          src={logolinkedin}
+          className="Footer-logo"
+          alt="logo-linkedin"
+        ></img>
+        <img src={logoyt} className="Footer-logo" alt="logo-yt"></img>
+      </div>
     </div>
   );
 }
