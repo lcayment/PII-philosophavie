62bd2c5dfea0af624915d37412788d2428f724b7
Lucie Cayment <lcayment@ensc.fr>
2022-03-01 16:49:49 +0100

    ajout side menu

--- src/component-const/Header.js
+++ src/component-const/Header.js
@@ -3,36 +3,60 @@ import "./Header.css";
 import logo from "../img/logo.jpeg";
 import logotw from "../img/icon-twitter.svg";
 import logofb from "../img/icon-fb.svg";
-import logolinkedin from "../img/icon-linkedin.svg";
+import logolk from "../img/icon-linkedin.svg";
 import logoyt from "../img/icon-yt.svg";
 
+import { useState } from "react";
+import { Menu } from "./Menu.js";
+import { ReactDimmer } from "react-dimmer";
+
 function Header() {
+  const [isMenuOpen, setMenu] = useState(false);
+
+  const handleMenu = () => {
+    setMenu((prevState) => !prevState);
+  };
+
   return (
-    <div className="Header">
-      <div className="Up-Header">
-        <div className="Header-Title">
-          <h1>Philosophavie</h1>
-          <p className="Header-Subtitle">Gabrielle Pozzo di Borgo</p>
-          <div className="Links">
-            <img src={logotw} className="Logo" alt="logo-tw"></img>
-            <img src={logofb} className="Logo" alt="logo-fb"></img>
-            <img src={logolinkedin} className="Logo" alt="logo-linkedin"></img>
-            <img src={logoyt} className="Logo" alt="logo-yt"></img>
+    <>
+      <div className="Header">
+        <div className="Up-Header">
+          <div className="Header-Title">
+            <h1>Philosophavie</h1>
+            <p className="Header-Subtitle">Gabrielle Pozzo di Borgo</p>
+            <div className="Links">
+              <img src={logotw} className="Logo" alt="logo-tw"></img>
+              <img src={logofb} className="Logo" alt="logo-fb"></img>
+              <img src={logolk} className="Logo" alt="logo-linkedin"></img>
+              <img src={logoyt} className="Logo" alt="logo-yt"></img>
+            </div>
+          </div>
+          <div>
+            <img src={logo} className="Header-logo" alt="logo" />
           </div>
         </div>
-        <div>
-          <img src={logo} className="Header-logo" alt="logo" />
+        <div className="Down-Header">
+          <div className="Menu">
+            <button className="menu-btn" onClick={handleMenu}>
+              Ouvrir menu
+            </button>
+            <div className="nav"></div>
+          </div>
+          <div className="Search">
+            <p>search</p>
+          </div>
         </div>
+      
+      <Menu isMenuOpen={isMenuOpen} />
+
+      <ReactDimmer
+        isOpen={isMenuOpen}
+        exitDimmer={setMenu}
+        zIndex={100}
+        blur={1.5}
+      />
       </div>
-      <div className="Down-Header">
-        <div className="Menu">
-          <p>hamburger</p>
-        </div>
-        <div className="Search">
-          <p>search</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
