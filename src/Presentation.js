df7e1b8afea8904456cfbeb44a4c6044b133b6ab
Lucie Cayment <lcayment@ensc.fr>
2022-03-02 08:39:17 +0100

    change tabs aspect in presentation

--- src/Presentation.js
+++ src/Presentation.js
@@ -8,7 +8,7 @@ function Presentation() {
   const [selectedTab, setSelectedTab] = useTabs(["me", "parcours", "vision"]);
   return (
     <div className="Presentation">
-      <nav className="flex border-b border-gray-500">
+      <nav className="tabs">
         <TabSelector
           isActive={selectedTab === "me"}
           onClick={() => setSelectedTab("me")}
