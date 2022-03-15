ed72a7f3498b22ebf6adddce4b28fae1ec41d087
Lucie Cayment <lcayment@ensc.fr>
2022-03-15 14:18:03 +0100

    v1 page projet/youtube ok

--- src/Projets/Pages/Youtube.js
+++ src/Projets/Pages/Youtube.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../Projets.css";
+import qrcodeyt from "../../img/qrcode-youtube.svg";
+import imgvideo from "../../img/imgvideo.png";
+
 import { Link } from "react-router-dom";
 
 function Projets() {
@@ -7,18 +10,31 @@ function Projets() {
     <div className="Projets">
       <h1 className="title">Youtube</h1>
       <div className="article">
-        Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-        cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-        reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure. Laborum
-        magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt
-        aliquip deserunt reprehenderit elit laborum. Nulla Lorem mollit
-        cupidatat irure. Laborum magna nulla duis ullamco cillum dolor.
-        Voluptate exercitation incididunt aliquip deserunt reprehenderit elit
-        laborum. Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
-        ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-        reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure. Laborum
-        magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt
-        aliquip deserunt reprehenderit elit laborum.
+        <div className="flashcode">
+          <img src={qrcodeyt} className="logo-qrcode" alt="qrcode-yt"></img>
+          <p>Flash moi pour te rendre sur la chaîne !</p>
+        </div>
+        <div>
+          <img
+            src={imgvideo}
+            className="imgvideotmp"
+            alt="image-video-tmp"
+          ></img>
+        </div>
+        <div>
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
+          cillum dolor. Voluptate exercitation incididunt aliquip deserunt
+          reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
+          Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation
+          incididunt aliquip deserunt reprehenderit elit laborum. Nulla Lorem
+          mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor.
+          Voluptate exercitation incididunt aliquip deserunt reprehenderit elit
+          laborum. Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+          ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+          deserunt reprehenderit elit laborum. Nulla Lorem mollit cupidatat
+          irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate
+          exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </div>
       </div>
     </div>
   );
