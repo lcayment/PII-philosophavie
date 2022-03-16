d41319e8926622258d32a1e445d9c1bcd72192b9
Lucie Cayment <lcayment@ensc.fr>
2022-03-16 11:42:57 +0100

    delete Home because it was useless, move all code into App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import "./App.css";
-import Home from "./Home/Home.js";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Header from "./ComposantsPrincipaux/Header.js";
+import Footer from "./ComposantsPrincipaux/Footer.js";
+import Sider from "./ComposantsPrincipaux/Sider.js";
+import Presentation from "./Presentation/Presentation.js";
+import Projets from "./Projets/Projets.js";
+import Actualites from "./Actualites/Actualites.js";
+import Boutique from "./Boutique/Boutique.js";
+import Contact from "./Contact/Contact.js";
+import Youtube from "./Projets/Pages/Youtube";
+import Instagram from "./Projets/Pages/Instagram";
+import Livres from "./Projets/Pages/Livres";
+import Interventions from "./Projets/Pages/Interventions";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Home/>
-      </Switch>
+      <Header />
+      <div className="App-Main">
+        <Switch>
+          <Route path="/presentation">
+            <Presentation />
+          </Route>
+          <Route path="/projets">
+            <Projets />
+          </Route>
+          <Route path="/actualites">
+            <Actualites />
+          </Route>
+          <Route path="/boutique">
+            <Boutique />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/youtube">
+            <Youtube />
+          </Route>
+          <Route path="/instagram">
+            <Instagram />
+          </Route>
+          <Route path="/livres">
+            <Livres />
+          </Route>
+          <Route path="/interventions">
+            <Interventions />
+          </Route>
+        </Switch>
+        <Sider />
+      </div>
+      <Footer />
     </Router>
   );
 }
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,57 +1,7 @@
 import "../App.css";
-import Header from "../ComposantsPrincipaux/Header.js";
-import Footer from "../ComposantsPrincipaux/Footer.js";
-import Sider from "../ComposantsPrincipaux/Sider.js";
-import Presentation from "../Presentation/Presentation.js";
-import Projets from "../Projets/Projets.js";
-import Actualites from "../Actualites/Actualites.js";
-import Boutique from "../Boutique/Boutique.js";
-import Contact from "../Contact/Contact.js";
-import Youtube from "../Projets/Pages/Youtube";
-import Instagram from "../Projets/Pages/Instagram";
-import Livres from "../Projets/Pages/Livres";
-import Interventions from "../Projets/Pages/Interventions";
-
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function Home() {
-  return (
-    <div className="Home">
-      <Header />
-      <div className="App-Main">
-        <Route path="/presentation">
-          <Presentation />
-        </Route>
-        <Route path="/projets">
-          <Projets />
-        </Route>
-        <Route path="/actualites">
-          <Actualites />
-        </Route>
-        <Route path="/boutique">
-          <Boutique />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/youtube">
-          <Youtube />
-        </Route>
-        <Route path="/instagram">
-          <Instagram />
-        </Route>
-        <Route path="/livres">
-          <Livres />
-        </Route>
-        <Route path="/interventions">
-          <Interventions />
-        </Route>
-        <Sider />
-      </div>
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
