ad64d9642cc3b6115d6aaff07e3135cbf0315087
Lucie Cayment <lcayment@ensc.fr>
2022-03-16 14:10:13 +0100

    readd tabs, ready to be redeploy

--- src/Presentation/Presentation.js
+++ src/Presentation/Presentation.js
@@ -1,35 +1,67 @@
 import React from "react";
 import "./Presentation.css";
-// import { TabPanel } from "../lib/index-navbar.tsx";
+//import { TabPanel } from "../lib/index-navbar.tsx";
 // import { useTabs } from "../lib/index-navbar.tsx";
 // import { TabSelector } from "../lib/TabSelector.tsx";
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-function Presentation() {
-  // const [selectedTab, setSelectedTab] = useTabs(["me", "parcours", "vision"]);
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Presentation() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="Presentation">
-      {/* <nav className="tabs">
-        <TabSelector
-          isActive={selectedTab === "me"}
-          onClick={() => setSelectedTab("me")}
-        >
-          Qui suis-je ?
-        </TabSelector>
-        <TabSelector
-          isActive={selectedTab === "parcours"}
-          onClick={() => setSelectedTab("parcours")}
-        >
-          Mon parcours
-        </TabSelector>
-        <TabSelector
-          isActive={selectedTab === "vision"}
-          onClick={() => setSelectedTab("vision")}
-        >
-          Ma vision
-        </TabSelector>
-      </nav> */}
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="basic tabs example"
+      >
+        <Tab label="Quis suis-je" {...a11yProps(0)} />
+        <Tab label="Mon parcours" {...a11yProps(1)} />
+        <Tab label="Ma vision" {...a11yProps(2)} />
+      </Tabs>
+
       <div className="presentation-content">
-        {/* <TabPanel hidden={selectedTab !== "me"}> */}
+        <TabPanel value={value} index={0}>
           <h1>Qui suis-je ?</h1>
           <p>
             Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
@@ -46,8 +78,8 @@ function Presentation() {
             cillum dolor. Voluptate exercitation incididunt aliquip deserunt
             reprehenderit elit laborum.
           </p>
-        {/* </TabPanel>
-        <TabPanel hidden={selectedTab !== "parcours"}> */}
+        </TabPanel>
+        <TabPanel value={value} index={1}>
           <h1>Mon parcours</h1>
           <p>
             Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
@@ -64,8 +96,8 @@ function Presentation() {
             cillum dolor. Voluptate exercitation incididunt aliquip deserunt
             reprehenderit elit laborum.
           </p>
-        {/* </TabPanel>
-        <TabPanel hidden={selectedTab !== "vision"}> */}
+        </TabPanel>
+        <TabPanel value={value} index={2}>
           <h1>Ma vision</h1>
           <p>
             Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
@@ -82,10 +114,8 @@ function Presentation() {
             cillum dolor. Voluptate exercitation incididunt aliquip deserunt
             reprehenderit elit laborum.
           </p>
-        {/* </TabPanel> */}
+        </TabPanel>
       </div>
     </div>
   );
 }
-
-export default Presentation;
