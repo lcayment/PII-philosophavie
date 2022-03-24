cf6ab1282885ceea4458f0b21cf11fb13f81c0ad
Lucie Cayment <lcayment@ensc.fr>
2022-03-24 15:56:35 +0100

    work with the packages

--- src/Presentation/Presentation.js
+++ src/Presentation/Presentation.js
@@ -1,117 +1,55 @@
 import React from "react";
 import "./Presentation.css";
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
 
 export default function Presentation() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
   return (
     <div className="Presentation">
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        aria-label="basic tabs example"
-      >
-        <Tab label="Quis suis-je" {...a11yProps(0)} />
-        <Tab label="Mon parcours" {...a11yProps(1)} />
-        <Tab label="Ma vision" {...a11yProps(2)} />
-      </Tabs>
-
       <div className="presentation-content">
-        <TabPanel value={value} index={0}>
-          <h1>Qui suis-je ?</h1>
-          <p>
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
-            Laborum magna nulla duis ullamco cillum dolor. Voluptate
-            exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
-            Laborum magna nulla duis ullamco cillum dolor. Voluptate
-            exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum.
-          </p>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <h1>Mon parcours</h1>
-          <p>
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
-            Laborum magna nulla duis ullamco cillum dolor. Voluptate
-            exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
-            Laborum magna nulla duis ullamco cillum dolor. Voluptate
-            exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum.
-          </p>
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <h1>Ma vision</h1>
-          <p>
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
-            Laborum magna nulla duis ullamco cillum dolor. Voluptate
-            exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
-            Laborum magna nulla duis ullamco cillum dolor. Voluptate
-            exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-            reprehenderit elit laborum.
-          </p>
-        </TabPanel>
+        <h1>Qui suis-je ?</h1>
+        <p>
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
+          cillum dolor. Voluptate exercitation incididunt aliquip deserunt
+          reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
+          Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation
+          incididunt aliquip deserunt reprehenderit elit laborum. Nulla Lorem
+          mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor.
+          Voluptate exercitation incididunt aliquip deserunt reprehenderit elit
+          laborum. Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+          ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+          deserunt reprehenderit elit laborum. Nulla Lorem mollit cupidatat
+          irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate
+          exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </p>
+        <h1>Mon parcours</h1>
+        <p>
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
+          cillum dolor. Voluptate exercitation incididunt aliquip deserunt
+          reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
+          Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation
+          incididunt aliquip deserunt reprehenderit elit laborum. Nulla Lorem
+          mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor.
+          Voluptate exercitation incididunt aliquip deserunt reprehenderit elit
+          laborum. Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+          ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+          deserunt reprehenderit elit laborum. Nulla Lorem mollit cupidatat
+          irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate
+          exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </p>
+        <h1>Ma vision</h1>
+        <p>
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
+          cillum dolor. Voluptate exercitation incididunt aliquip deserunt
+          reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
+          Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation
+          incididunt aliquip deserunt reprehenderit elit laborum. Nulla Lorem
+          mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor.
+          Voluptate exercitation incididunt aliquip deserunt reprehenderit elit
+          laborum. Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+          ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+          deserunt reprehenderit elit laborum. Nulla Lorem mollit cupidatat
+          irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate
+          exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </p>
       </div>
     </div>
   );
