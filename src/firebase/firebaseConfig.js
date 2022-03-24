981c8a05d4cff03ef62d51b7a339070b85b94a63
Lucie Cayment <lcayment@ensc.fr>
2022-03-24 16:18:58 +0100

    change .env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCidXUtohJbaaB1tCgtMIxWsIWrDKmSjUQ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
