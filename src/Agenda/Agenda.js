93f521884d166252763fdf44ef3164b7ef5fe6cc
Lucie Cayment <lcayment@ensc.fr>
2022-04-12 10:21:11 +0200

    add event to firebase, but without date

--- src/Agenda/Agenda.js
+++ src/Agenda/Agenda.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./Agenda.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { FaPlus } from "react-icons/fa";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "@firebase/firestore";
+import { db } from "../firebase/firebaseConfig";
+
 import { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
@@ -30,14 +41,38 @@ const events = [
 function Agenda() {
   const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
   const [allEvents, setAllEvents] = useState(events);
+  const eventCollectionRef = collection(db, "agenda");
 
   function handleAddEvent() {
     setAllEvents([...allEvents, newEvent]);
   }
 
+  const createEvent = async () => {
+    await addDoc(
+      eventCollectionRef,
+      {
+        event: newEvent,
+      },
+      [eventCollectionRef]
+    );
+  };
+
   return (
     <div className="Agenda">
       <h1>Agenda</h1>
+      <div>
+        <input
+          placeholder={"Ajoutez le titre de l'evenement"}
+          onChange={(event) => {
+            setNewEvent(event.target.value);
+          }}
+        />
+        <div className="div-btn">
+          <button className="CRUD-btn" onClick={createEvent}>
+            <FaPlus />
+          </button>
+        </div>
+      </div>
       <Calendar
         localizer={localizer}
         events={allEvents}
