7d7ce9fc5ae8150e62b7f36c14d3c8412e4978c5
Lucie Cayment <lcayment@ensc.fr>
2022-04-12 10:07:14 +0200

    first step agenda working, display of the calendar with few event

--- src/Agenda/Agenda.js
+++ src/Agenda/Agenda.js
@@ -3,29 +3,48 @@ import "./Agenda.css";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
 
 const localizer = momentLocalizer(moment);
 
-class Agenda extends Component() {
-  state = {
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment().add(1, "days").toDate(),
-        title: "Some title",
-      },
-    ],
-  };
-  return() {
+const events = [
+  {
+    title: "Big Meeting",
+    allDay: true,
+    start: new Date(2021, 6, 0),
+    end: new Date(2021, 6, 0),
+  },
+  {
+    title: "Vacation",
+    start: new Date(2021, 6, 7),
+    end: new Date(2021, 6, 10),
+  },
+  {
+    title: "Conference",
+    start: new Date(2021, 6, 20),
+    end: new Date(2021, 6, 23),
+  },
+];
+
+function Agenda() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  function handleAddEvent() {
+    setAllEvents([...allEvents, newEvent]);
+  }
+
+  return (
     <div className="Agenda">
       <Calendar
         localizer={localizer}
-        defaultDate={new Date()}
-        defaultView="month"
-        style={{ height: "100vh" }}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }}
       />
-    </div>;
-  }
+    </div>
+  );
 }
 
 export default Agenda;
