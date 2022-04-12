1aecb5940758e5e18fca8681c65d5b389c417fc8
Lucie Cayment <lcayment@ensc.fr>
2022-04-12 16:28:14 +0200

    add crud to pres projects pages

--- src/Projets/Projets.js
+++ src/Projets/Projets.js
@@ -1,4 +1,7 @@
-import React from "react";
+import { collection, getDocs, updateDoc, doc } from "@firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase/firebaseConfig";
+
 import "./Projets.css";
 import { Link } from "react-router-dom";
 import { FaYoutube } from "react-icons/fa";
@@ -6,32 +9,90 @@ import { FaInstagram } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 import { HiOutlineSpeakerphone } from "react-icons/hi";
+import { auth } from "../firebase/firebaseConfig";
+import Collapsible from "react-collapsible";
+import { FaPencilAlt } from "react-icons/fa";
 
 function Projets() {
+  const [newProjet, setNewProjet] = useState("Projets");
+  const [projet, setProjet] = useState([]);
+  const projetCollectionRef = collection(db, "projet");
+
+  const updateProjet = async (id, proj) => {
+    const projetDoc = doc(db, "projet", id);
+    const newFields = { proj: newProjet }; // ajouter le field a modifier
+    await updateDoc(projetDoc, newFields);
+  };
+
+  // render each time the page is called
+  useEffect(() => {
+    const getProjet = async () => {
+      const data = await getDocs(projetCollectionRef);
+      setProjet(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getProjet();
+  }, [projetCollectionRef]);
+
+  const user = auth.currentUser;
+
   return (
     <div className="Projets">
-      <h1 className="title">Projets</h1>
-      <div className="article">
-        Vous pouvez retrouver ici tous les projets que je mène en ligne.
-        J’espère vous retrouver dans les commentaires !
-      </div>
-      <div className="logo-display">
-        <Link className="logo" to="/youtube">
-          <FaYoutube />
-        </Link>
-        <Link className="logo" to="/instagram">
-          <FaInstagram />
-        </Link>
-        <Link className="logo" to="/livres">
-          <FaBook />
-        </Link>
-        <Link className="logo" to="/interventions">
-          <HiOutlineSpeakerphone />
-        </Link>
-        <Link className="logo" to="/tiktok">
-          <FaTiktok />
-        </Link>
-      </div>
+      {projet.map((proj) => {
+        return (
+          <div>
+            <h1 className="title">Projets</h1>
+            <div className="article">{proj.proj}</div>
+            {user ? (
+              <Collapsible
+                trigger="Modifier la partie Qui suis-je ?"
+                triggerClassName="collapse"
+                triggerOpenedClassName="collapse"
+              >
+                <div className="change-pres">
+                  <div>
+                    <textarea
+                      placeholder="Modification du contenu du qui suis-je"
+                      onChange={(event) => {
+                        setNewProjet(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="div-btn">
+                    <button
+                      className="CRUD-btn"
+                      onClick={() => {
+                        updateProjet(proj.id, proj.proj);
+                      }}
+                    >
+                      <FaPencilAlt />
+                    </button>
+                  </div>
+                </div>
+              </Collapsible>
+            ) : (
+              ""
+            )}
+            <div className="logo-display">
+              <Link className="logo" to="/youtube">
+                <FaYoutube />
+              </Link>
+              <Link className="logo" to="/instagram">
+                <FaInstagram />
+              </Link>
+              <Link className="logo" to="/livres">
+                <FaBook />
+              </Link>
+              <Link className="logo" to="/interventions">
+                <HiOutlineSpeakerphone />
+              </Link>
+              <Link className="logo" to="/tiktok">
+                <FaTiktok />
+              </Link>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
