6b1e9f9bf84a02bb2a7982317d8085c1b84de0bd
Lucie Cayment <lcayment@ensc.fr>
2022-04-19 08:57:07 +0200

    add CRUD on tiktok and youtube pages

--- src/Projets/Pages/TikTok.js
+++ src/Projets/Pages/TikTok.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Projets.css";
+
+// firestore
+import { collection, updateDoc, getDocs, doc } from "@firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
+import { auth } from "../../firebase/firebaseConfig";
 
 // navigation
 import { Link } from "react-router-dom";
@@ -10,9 +15,34 @@ import { FaInstagram } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 import { HiOutlineSpeakerphone } from "react-icons/hi";
+import { FaPencilAlt } from "react-icons/fa";
 import tiktok from "../../img/tiktok.png";
 
-function Projets() {
+// components
+import Collapsible from "react-collapsible";
+
+export default function Projets() {
+  const [newProjet, setNewProjet] = useState("Youtube");
+  const [projet, setProjet] = useState([]);
+  const projetCollectionRef = collection(db, "projet");
+
+  const updateProjet = async (id, tiktok) => {
+    const projetDoc = doc(db, "projet", id);
+    const newFields = { tiktok: newProjet }; // ajouter le field a modifier
+    await updateDoc(projetDoc, newFields);
+  };
+
+  //render each time the page is called
+  useEffect(() => {
+    const getProjet = async () => {
+      const data = await getDocs(projetCollectionRef);
+      setProjet(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getProjet();
+  }, [projetCollectionRef]);
+
+  const user = auth.currentUser;
+
   return (
     <div className="Projets">
       <h1 className="title">Tiktok</h1>
@@ -33,30 +63,54 @@ function Projets() {
           <FaTiktok />
         </Link>
       </div>
-      <div className="article-row">
-        <div className="article-two">
-          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-          cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-          reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
-          Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation
-          incididunt aliquip deserunt reprehenderit elit laborum. Nulla Lorem
-          mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor.
-          Voluptate exercitation incididunt aliquip deserunt reprehenderit elit
-          laborum. Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
-          ullamco cillum dolor. Voluptate exercitation incididunt aliquip
-          deserunt reprehenderit elit laborum. Nulla Lorem mollit cupidatat
-          irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate
-          exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-        </div>
-        <div>
-          <div className="margin">
-            <img src={tiktok} alt="post-tiktok"></img>
-            <p>Clique pour voir mon dernier post tiktok !</p>
+      {projet.map((proj) => {
+        return (
+          <div>
+            <div className="article-row">
+              <div className="article-two">{proj.tiktok}</div>
+              <div>
+                <div className="margin">
+                  <img src={tiktok} alt="post-tiktok"></img>
+                  <p>Clique pour voir mon dernier post tiktok !</p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              {user ? (
+                <Collapsible
+                  trigger="Modifier la présentation de tiktok"
+                  triggerClassName="collapse"
+                  triggerOpenedClassName="collapse"
+                >
+                  <div className="change-pres">
+                    <div>
+                      <textarea
+                        placeholder="Modification du texte sur tiktok"
+                        onChange={(event) => {
+                          setNewProjet(event.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="div-btn">
+                      <button
+                        className="CRUD-btn"
+                        onClick={() => {
+                          updateProjet(proj.id, proj.tiktok);
+                        }}
+                      >
+                        <FaPencilAlt />
+                      </button>
+                    </div>
+                  </div>
+                </Collapsible>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
-
-export default Projets;
--- src/Projets/Pages/Youtube.js
+++ src/Projets/Pages/Youtube.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Projets.css";
+
+// firestore
+import { collection, updateDoc, getDocs, doc } from "@firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
+import { auth } from "../../firebase/firebaseConfig";
 
 // navigation
 import { Link } from "react-router-dom";
@@ -10,12 +15,33 @@ import { FaInstagram } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 import { HiOutlineSpeakerphone } from "react-icons/hi";
+import { FaPencilAlt } from "react-icons/fa";
 import qrcodeyt from "../../img/qrcode-youtube.svg";
 
 // components
+import Collapsible from "react-collapsible";
 import YouTube from "react-youtube";
 
-function Projets() {
+export default function Projets() {
+  const [newProjet, setNewProjet] = useState("Youtube");
+  const [projet, setProjet] = useState([]);
+  const projetCollectionRef = collection(db, "projet");
+
+  const updateProjet = async (id, youtube) => {
+    const projetDoc = doc(db, "projet", id);
+    const newFields = { youtube: newProjet }; // ajouter le field a modifier
+    await updateDoc(projetDoc, newFields);
+  };
+
+  //render each time the page is called
+  useEffect(() => {
+    const getProjet = async () => {
+      const data = await getDocs(projetCollectionRef);
+      setProjet(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getProjet();
+  }, [projetCollectionRef]);
+
   const videoOptions = {
     playerVars: {
       autoplay: 0,
@@ -26,6 +52,8 @@ function Projets() {
       loop: 0,
     },
   };
+
+  const user = auth.currentUser;
 
   return (
     <div className="Projets">
@@ -47,33 +75,58 @@ function Projets() {
           <FaTiktok />
         </Link>
       </div>
-      <div className="article">
-        <div className="article-one">
-          <div className="flashcode">
-            <img src={qrcodeyt} className="petit-logo" alt="qrcode-yt"></img>
-            <p>Flash moi pour te rendre sur la chaîne !</p>
+      {projet.map((proj) => {
+        return (
+          <div>
+            <div className="article">
+              <div className="article-one">
+                <div className="flashcode">
+                  <img
+                    src={qrcodeyt}
+                    className="petit-logo"
+                    alt="qrcode-yt"
+                  ></img>
+                  <p>Flash moi pour te rendre sur la chaîne !</p>
+                </div>
+                <div className="video">
+                  <YouTube videoId="8vpXdiAI8E0" opts={videoOptions} />
+                </div>
+              </div>
+              <div className="article-one">{proj.youtube}</div>
+            </div>
+            {user ? (
+              <Collapsible
+                trigger="Modifier la présentation de youtube"
+                triggerClassName="collapse"
+                triggerOpenedClassName="collapse"
+              >
+                <div className="change-pres">
+                  <div>
+                    <textarea
+                      placeholder="Modification du texte sur youtube"
+                      onChange={(event) => {
+                        setNewProjet(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="div-btn">
+                    <button
+                      className="CRUD-btn"
+                      onClick={() => {
+                        updateProjet(proj.id, proj.youtube);
+                      }}
+                    >
+                      <FaPencilAlt />
+                    </button>
+                  </div>
+                </div>
+              </Collapsible>
+            ) : (
+              ""
+            )}
           </div>
-          <div className="video">
-            <YouTube videoId="8vpXdiAI8E0" opts={videoOptions} />
-          </div>
-        </div>
-        <div className="article-one">
-          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-          cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-          reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure.
-          Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation
-          incididunt aliquip deserunt reprehenderit elit laborum. Nulla Lorem
-          mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor.
-          Voluptate exercitation incididunt aliquip deserunt reprehenderit elit
-          laborum. Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
-          ullamco cillum dolor. Voluptate exercitation incididunt aliquip
-          deserunt reprehenderit elit laborum. Nulla Lorem mollit cupidatat
-          irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate
-          exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
-
-export default Projets;
